c18ea906b7a9e42a964d9019287c00f5cb58d4c2
jmp613 <matthew.pistorius@gmail.com>
2013-05-20 00:58:58 -0500

    Add Recipe Controller

---
+++ src/BrewersBuddy/Models/RecipeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BrewersBuddy;
+using BrewersBuddy.Controllers;
+
+namespace BrewersBuddy.Models
+{
+    public class RecipeController : Controller
+    {
+        //
+        // GET: /Default1/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
