b8a85357d32c26b7862af883065c68678077f763
Jon Parise <jlparise@gmail.com>
2013-05-26 18:56:28 -0400

    Adds a controller for batches. Addes some tests for this controller that do not yet pass.

---
+++ src/BrewersBuddy.Tests/Controllers/BatchContollerTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using BrewersBuddy.Models;
+using BrewersBuddy.Controllers;
+
+using BrewersBuddy.Tests.Utils;
+using System.Web.Mvc;
+
+namespace BrewersBuddy.Tests.Controllers
+{
+    [TestClass]
+    public class BatchContollerTest
+    {
+        
+
+        [TestMethod]
+        public void TestBatchList()
+        {
+            UserProfile user = TestUtils.createUser(111, "Mike", "Smith");
+            ICollection<Batch> batches = TestUtils.createBatches(5, user);
+
+            BatchController controller = new BatchController();
+            ViewResult result = (ViewResult)controller.Index();
+            ViewDataDictionary data = result.ViewData;
+
+            Assert.IsTrue(data.Count == 5);
+
+            foreach (Batch batch in batches)
+            {
+                //Assert.IsTrue(data.Contains(batch));
+            }
+        }
+    }
+}
--- src/BrewersBuddy.Tests/Models/BatchTests.cs
+++ src/BrewersBuddy.Tests/Models/BatchTests.cs
@@ -9,6 +9,24 @@ namespace BrewersBuddy.Tests.Models
     [TestClass]
     public class BatchTests
     {
+
+
+        [TestMethod]
+        public void TestCreateBatch()
+        {
+            BatchDBContext db = new BatchDBContext();
+
+            Batch batch = new Batch();
+            batch.Name = "Test";
+            batch.Type = BatchType.Mead;
+            batch.StartDate = DateTime.Now;
+
+            db.Batches.Add(batch);
+
+            //DbSet<Batch> batches = db.Batches;
+            Assert.Fail("Not sure why the abve line isn;t working. Referneces??");
+        }
+
         [TestMethod]
         public void TestAddMeasurement()
         {
@@ -35,6 +53,34 @@ namespace BrewersBuddy.Tests.Models
 
 
         [TestMethod]
+        public void TestRemoveMeasurement()
+        {
+            BatchDBContext db = new BatchDBContext();
+
+            Batch batch = new Batch();
+            batch.Name = "Test";
+            batch.Type = BatchType.Wine;
+            batch.StartDate = DateTime.Now;
+
+            db.Batches.Add(batch);
+
+            Measurement measurement = new Measurement();
+            measurement.Batch = batch;
+            measurement.Description = "This is a test!";
+            measurement.Measured = "Gravity";
+            measurement.Value = 1.01;
+            measurement.MeasurementDate = DateTime.Now;
+
+            db.Measurements.Add(measurement);
+
+            Assert.IsTrue(batch.Measurements.Contains(measurement));
+
+            batch.Measurements.Remove(measurement);
+
+            Assert.IsFalse(batch.Measurements.Contains(measurement));
+        }
+
+        [TestMethod]
         public void TestAddBatchAction()
         {
             BatchDBContext db = new BatchDBContext();
@@ -56,6 +102,35 @@ namespace BrewersBuddy.Tests.Models
             db.BatchActions.Add(action);
 
             Assert.IsTrue(batch.Actions.Contains(action));
+        }
+
+
+        [TestMethod]
+        public void TestRemoveBatchAction()
+        {
+            BatchDBContext db = new BatchDBContext();
+
+            Batch batch = new Batch();
+            batch.Name = "Test";
+            batch.Type = BatchType.Beer;
+            batch.StartDate = DateTime.Now;
+
+            db.Batches.Add(batch);
+
+            BatchAction action = new BatchAction();
+            action.Batch = batch;
+            action.Description = "99 bottles of beer on the wall!";
+            action.Type = ActionType.Bottle;
+            action.Title = "Bottles the beer";
+            action.ActionDate = DateTime.Now;
+
+            db.BatchActions.Add(action);
+
+            Assert.IsTrue(batch.Actions.Contains(action));
+
+            db.BatchActions.Remove(action);
+
+            Assert.IsFalse(batch.Actions.Contains(action));
         }
 
 
@@ -89,6 +164,12 @@ namespace BrewersBuddy.Tests.Models
             db.Cellars.Add(cellar);
 
             Assert.IsTrue(cellar.Containers.Contains(container));
+        }
+
+        [TestMethod]
+        public void TestViewBacthActions()
+        {
+            
         }
     }
 }
