e5116f980ff64f1a2e048aca97977fd94737a859
Jon Parise <jlparise@gmail.com>
2013-06-30 19:51:54 -0400

    Add details page for batch notes. Still not sure it is working, but I don't want to keep the project file checked out.

---
+++ src2/BrewersBuddy/Controllers/BatchNoteController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BrewersBuddy.Models;
+
+namespace BrewersBuddy.Controllers
+{
+    public class BatchNoteController : Controller
+    {
+        private BrewersBuddyContext db = new BrewersBuddyContext();
+
+
+        //
+        // GET: /BatchNote/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            BatchNote batchnote = db.BatchNotes.Find(id);
+            if (batchnote == null)
+            {
+                return HttpNotFound();
+            }
+            return View(batchnote);
+        }
+
+
+        //
+        // GET: /BatchNote/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            BatchNote batchnote = db.BatchNotes.Find(id);
+            if (batchnote == null)
+            {
+                return HttpNotFound();
+            }
+            return View(batchnote);
+        }
+
+        //
+        // POST: /BatchNote/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(BatchNote batchnote)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(batchnote).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(batchnote);
+        }
+}
