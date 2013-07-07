6f51407a38aae3b379463f9a418edc4f37e5dcb3
Jon Parise <jlparise@gmail.com>
2013-07-07 16:05:40 -0400

    Edit, view and delete  of measurements works now. fixes #55 fixes #56 fixes #57

---
+++ src2/BrewersBuddy/Controllers/MeasurementController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BrewersBuddy.Models;
+
+namespace BrewersBuddy.Controllers
+{
+    [Authorize]
+    public class MeasurementController : Controller
+    {
+        private BrewersBuddyContext db = new BrewersBuddyContext();
+
+        //
+        // GET: /Measurment/
+
+        public ActionResult Index()
+        {
+            return View(db.Measurements.ToList());
+        }
+
+        //
+        // GET: /Measurment/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Measurement measurement = db.Measurements.Find(id);
+            if (measurement == null)
+            {
+                return HttpNotFound();
+            }
+            return View(measurement);
+        }
+
+        //
+        // GET: /Measurment/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Measurement measurement = db.Measurements.Find(id);
+            if (measurement == null)
+            {
+                return HttpNotFound();
+            }
+            return View(measurement);
+        }
+
+        //
+        // POST: /Measurment/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Measurement measurement)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(measurement).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(measurement);
+        }
+
+        //
+        // GET: /Measurment/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Measurement measurement = db.Measurements.Find(id);
+            if (measurement == null)
+            {
+                return HttpNotFound();
+            }
+            return View(measurement);
+        }
+
+        //
+        // POST: /Measurment/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Measurement measurement = db.Measurements.Find(id);
+            db.Measurements.Remove(measurement);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
