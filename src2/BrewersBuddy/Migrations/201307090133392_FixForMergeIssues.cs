789379ca46a1542d5c26175de4d3d49edd4f31b5
Steve Platz <steve.platz@gmail.com>
2013-07-08 21:34:57 -0400

    Fix for stupid entity framework

---
+++ src2/BrewersBuddy/Migrations/201307090133392_FixForMergeIssues.cs
@@ -0,0 +1,17 @@
+namespace BrewersBuddy.Migrations
+{
+    using System.Data.Entity.Migrations;
+
+    public partial class FixForMergeIssues : DbMigration
+    {
+        public override void Up()
+        {
+            // I hate Entity Framework. It is dumb and there's no need for this migration.
+        }
+
+        public override void Down()
+        {
+
+        }
+    }
+}
