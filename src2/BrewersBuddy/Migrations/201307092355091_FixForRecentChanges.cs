1eaef97c3f7414fbe116298a989685bc4df8b0e1
Steve Platz <steve.platz@gmail.com>
2013-07-09 19:58:48 -0400

    Fixing another entity framework bug/issue/problem

---
+++ src2/BrewersBuddy/Migrations/201307092355091_FixForRecentChanges.cs
@@ -0,0 +1,17 @@
+namespace BrewersBuddy.Migrations
+{
+    using System.Data.Entity.Migrations;
+
+    public partial class FixForRecentChanges : DbMigration
+    {
+        public override void Up()
+        {
+            // Stupid, stupid entity framework. I hate you so.
+        }
+
+        public override void Down()
+        {
+
+        }
+    }
+}
