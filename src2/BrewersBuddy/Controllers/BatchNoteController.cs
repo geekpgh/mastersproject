077aa26e9c39961e12b73b8276855691d9cf5abb
GIdeus <gui102@mail.com>
2013-07-12 18:30:34 +0200

    moved edit/delete note to batchnote controller
    moved edit/delete note from batch controller to batchnote controller to
    work with new batch view

--- src2/BrewersBuddy/Controllers/BatchNoteController.cs
+++ src2/BrewersBuddy/Controllers/BatchNoteController.cs
@@ -7,13 +7,19 @@ namespace BrewersBuddy.Controllers
 {
     public class BatchNoteController : Controller
     {
+        private readonly IBatchService _batchService;
         private readonly IBatchNoteService _noteService;
 
-        public BatchNoteController(IBatchNoteService noteService)
+        public BatchNoteController(
+            IBatchService batchService,
+            IBatchNoteService noteService)
         {
+            if (batchService == null)
+                throw new ArgumentNullException("batchService");
             if (noteService == null)
                 throw new ArgumentNullException("noteService");
 
+            _batchService = batchService;
             _noteService = noteService;
         }
 
@@ -30,33 +36,58 @@ namespace BrewersBuddy.Controllers
             return View(batchnote);
         }
 
-
         //
         // GET: /BatchNote/Edit/5
 
         public ActionResult Edit(int id = 0)
         {
-            BatchNote batchnote = _noteService.Get(id);
-            if (batchnote == null)
+            BatchNote note = _noteService.Get(id);
+            if (note == null)
             {
                 return HttpNotFound();
             }
-            return View(batchnote);
+
+            return View(note);
         }
 
-        //
-        // POST: /BatchNote/Edit/5
 
+        //
+        // POST: /Batch/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(BatchNote batchNote)
+        public ActionResult Edit(BatchNote note)
         {
             if (ModelState.IsValid)
             {
-                _noteService.Update(batchNote);
-                return RedirectToAction("Index");
+                _noteService.Update(note);
+                return RedirectToAction("Details/" + note.BatchId, "Batch");
             }
-            return View(batchNote);
+            return View(note);
+        }
+
+
+        //
+        // GET: /Batch/Delete/5
+        public ActionResult Delete(int id = 0)
+        {
+
+            BatchNote note = _noteService.Get(id);
+            if (note == null)
+            {
+                return HttpNotFound();
+            }
+            return View(note);
+        }
+
+        //
+        // POST: /Batch/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteNoteConfirmed(int id = 0)
+        {
+            BatchNote note = _noteService.Get(id);
+            _noteService.Delete(note);
+            return RedirectToAction("Details/" + note.BatchId, "Batch");
         }
     }
 }
