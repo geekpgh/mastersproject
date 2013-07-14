f5b27658a9f14819f6f15e947fd0376ce3eda0ee
GIdeus <gui102@mail.com>
2013-07-14 14:21:26 +0200

    Added a FAQ
    I started a FAQ, as you are going through coding if you see something
    that could be added please include it.

---
+++ src2/BrewersBuddy/Controllers/CellarController.cs
@@ -0,0 +1,21 @@
+﻿using BrewersBuddy.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BrewersBuddy.Controllers
+{
+    public class CellarController : Controller
+    {
+        //
+        // GET: /Cellar/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ src2/BrewersBuddy/Controllers/HelpController.cs
@@ -0,0 +1,23 @@
+﻿using BrewersBuddy.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BrewersBuddy.Controllers
+{
+    public class HelpController : Controller
+    {
+        public ActionResult FAQ()
+        {
+            return View();
+        }
+
+        public ActionResult Manual()
+        {
+            return View();
+        }
+
+    }
+}
