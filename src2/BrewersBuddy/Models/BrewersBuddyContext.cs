ef6823e16a954c6ed572c029c5a264e4e1f96876
Geoff Blogref <gmblogref@hotmail.com>
2013-07-21 20:04:49 -0500

    adding FAQ's

--- src2/BrewersBuddy/Models/BrewersBuddyContext.cs
+++ src2/BrewersBuddy/Models/BrewersBuddyContext.cs
@@ -3,37 +3,42 @@ using System.Data.Entity.ModelConfiguration.Conventions;
 
 namespace BrewersBuddy.Models
 {
-    public class BrewersBuddyContext : DbContext
-    {
-        public BrewersBuddyContext()
-            : base("DefaultConnection")
-        {
-        }
+	public class BrewersBuddyContext : DbContext
+	{
+		public BrewersBuddyContext()
+			: base("DefaultConnection")
+		{
+		}
 
-        public DbSet<Batch> Batches { get; set; }
-        public DbSet<BatchNote> BatchNotes { get; set; }
-        public DbSet<BatchAction> BatchActions { get; set; }
-        public DbSet<Measurement> Measurements { get; set; }
-        public DbSet<Recipe> Recipes { get; set; }
-        public DbSet<Ingredient> Ingredients { get; set; }
-        public DbSet<Container> Containers { get; set; }
-        public DbSet<BatchRating> BatchRatings { get; set; }
-        public DbSet<BatchComment> BatchComments { get; set; }
+		public DbSet<Batch> Batches { get; set; }
+		public DbSet<BatchNote> BatchNotes { get; set; }
+		public DbSet<BatchAction> BatchActions { get; set; }
+		public DbSet<Measurement> Measurements { get; set; }
+		public DbSet<Recipe> Recipes { get; set; }
+		public DbSet<Ingredient> Ingredients { get; set; }
+		public DbSet<Container> Containers { get; set; }
+		public DbSet<BatchRating> BatchRatings { get; set; }
+		public DbSet<BatchComment> BatchComments { get; set; }
 
-        public DbSet<UserProfile> UserProfiles { get; set; }
+		public DbSet<UserProfile> UserProfiles { get; set; }
 		public DbSet<webpages_Membership> webpages_Memberships { get; set; }
 
-        protected override void OnModelCreating(DbModelBuilder modelBuilder)
-        {
+		protected override void OnModelCreating(DbModelBuilder modelBuilder)
+		{
 			base.OnModelCreating(modelBuilder);
-            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+			modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
 			modelBuilder.Entity<Ingredient>()
 				.HasMany(c => c.Recipes)
 				.WithMany(i => i.Ingredients)
 				.Map(t => t.MapLeftKey("IngredientID")
 					.MapRightKey("RecipeID")
 					.ToTable("IngredientRecipe"));
-        }
 
-    }
+			modelBuilder.Entity<UserProfile>()
+				.HasRequired(t => t.BatchRatings)
+				.WithOptional()
+				.WillCascadeOnDelete(false);
+		}
+
+	}
 }
