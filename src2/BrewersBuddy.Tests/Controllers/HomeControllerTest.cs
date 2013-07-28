8789279c7169ff4e717b13e92a26c27753e3f883
John Matthew Pistorius <matthew.pistorius@gmail.com>
2013-07-28 09:38:44 -0500

    Revert "Removing default test which fails, and updating the proj file."
    This reverts commit eec6adbb5b1e6f817bf52bc2992e1b347f42925b.

---
+++ src2/BrewersBuddy.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using NUnit.Framework;
+using BrewersBuddy;
+using BrewersBuddy.Controllers;
+using NSubstitute;
+using System.Web;
+using System.Web.Routing;
+
+namespace BrewersBuddy.Tests.Controllers
+{
+    [TestFixture]
+    public class HomeControllerTest : DbTestBase
+    {
+        [Test]
+        public void Index()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.AreEqual("Modify this template to jump-start your ASP.NET MVC application.", result.ViewBag.Message);
+        }
+    }
+}
