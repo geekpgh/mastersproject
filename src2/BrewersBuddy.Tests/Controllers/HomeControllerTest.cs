fa5bb3ca7d1fcb9243cfa132091447e8e1857ade
Steve Platz <steve.platz@gmail.com>
2013-07-28 17:02:46 -0400

    Including test file since it wasn't automatically included with the last commit.

---
+++ src2/BrewersBuddy.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using BrewersBuddy.Controllers;
+using BrewersBuddy.Models;
+using WebMatrix.WebData;
+using NUnit.Framework;
+using NSubstitute;
+using BrewersBuddy.Services;
+using System.Collections.Generic;
+using System;
+
+namespace BrewersBuddy.Tests.Controllers
+{
+    [TestFixture]
+    public class HomeControllerTest
+    {
+        [Test]
+        public void TestIndexDoesNotRequireAuthentication()
+        {
+            Type type = typeof(HomeController);
+            Attribute[] classAttributes = Attribute.GetCustomAttributes(type, typeof(AuthorizeAttribute));
+
+            Assert.AreEqual(0, classAttributes.Length);
+
+            object[] methodAttributes = type.GetMethod("Index")
+                .GetCustomAttributes(typeof(AuthorizeAttribute), true);
+
+            Assert.AreEqual(0, methodAttributes.Length);
+        }
+    }
+}
