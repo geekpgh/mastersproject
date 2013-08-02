b3ec8337a4c2285005ee8f49c053a39a6c2cb023
Steve Platz <steve.platz@gmail.com>
2013-08-01 21:56:23 -0400

    Cleans up all friends/account page UIs (resolves #83)

--- src2/BrewersBuddy/Models/AccountModels.cs
+++ src2/BrewersBuddy/Models/AccountModels.cs
@@ -11,9 +11,12 @@ namespace BrewersBuddy.Models
         [Key]
         [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
         public int UserId { get; set; }
+        [Display(Name = "User Name")]
         public string UserName { get; set; }
         public string Email { get; set; }
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
         public string City { get; set; }
 
