4d26619bcce01f4e4d37b24a3c241162c9d99288
Steve Platz <steve.platz@gmail.com>
2013-08-05 19:06:59 -0400

    Removes migration for membership table

--- src2/BrewersBuddy/Migrations/201308051948159_Initial.cs
+++ src2/BrewersBuddy/Migrations/201308051948159_Initial.cs
@@ -186,24 +186,6 @@ namespace BrewersBuddy.Migrations
                 .PrimaryKey(t => t.IngredientId);
             
             CreateTable(
-                "dbo.webpages_Membership",
-                c => new
-                    {
-                        UserId = c.Int(nullable: false, identity: true),
-                        CreateDate = c.DateTime(nullable: false),
-                        ConfirmationToken = c.String(),
-                        IsConfirmed = c.Boolean(nullable: false),
-                        LastPasswordFailureDate = c.DateTime(nullable: false),
-                        PasswordFailuresSinceLastSuccess = c.Int(nullable: false),
-                        Password = c.String(),
-                        PasswordChangeDate = c.DateTime(nullable: false),
-                        PasswordSalt = c.String(),
-                        PasswordVerificationToken = c.String(),
-                        PasswordVerificationTokenExpirationDate = c.DateTime(nullable: false),
-                    })
-                .PrimaryKey(t => t.UserId);
-            
-            CreateTable(
                 "dbo.BatchCollaborator",
                 c => new
                     {
@@ -275,7 +257,6 @@ namespace BrewersBuddy.Migrations
             DropForeignKey("dbo.Batch", "OwnerId", "dbo.UserProfile");
             DropTable("dbo.RecipeIngredient");
             DropTable("dbo.BatchCollaborator");
-            DropTable("dbo.webpages_Membership");
             DropTable("dbo.Ingredient");
             DropTable("dbo.Recipe");
             DropTable("dbo.Container");
